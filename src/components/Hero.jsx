e1e46ba16f2e8e1420635e3b713118ff6f0f66b1
vargaae <vargaae@hotmail.com>
2024-02-25 16:29:05 +0100

    104dev-ChatGPT update description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/vargaae/ai-2024", "_blank")
           }
           className='black_btn'
         >
