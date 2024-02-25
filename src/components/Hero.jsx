c65ea4b4366a6b8d65f5b5088180e49ba8434b17
vargaae <vargaae@hotmail.com>
2024-02-25 16:20:08 +0100

    103dev-ChatGPT add logos theme

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+        <span className='blue_gradient '>OpenAI GPT-4</span>
       </h1>
       <h2 className='desc'>
         Simplify your reading with Summize, an open-source article summarizer
