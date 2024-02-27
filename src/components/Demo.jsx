8c4ca7363de49dc795d1160980ff73b256c297fa
vargaae <vargaae@hotmail.com>
2024-02-27 02:56:38 +0100

    113dev-ChatGPT Copy to Clipboard

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -10,6 +10,7 @@ const Demo = () => {
   });
 
   const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
@@ -39,6 +40,12 @@ const Demo = () => {
 
       console.log(newArticle);
     }
+  };
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
   };
 
   return (
@@ -79,9 +86,9 @@ const Demo = () => {
               onClick={() => setArticle(item)}
               className="link_card"
             >
-              <div className="copy_btn">
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                 <img
-                  src={copy}
+                  src={copied === item.url ? tick : copy}
                   alt="copy icon"
                   className="w-[40%] h-[40%] object-contain"
                 />
